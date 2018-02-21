10126783c01569ab8a550280b2510f67d128dd55
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-02-20 20:44:13 -0800

    working on js still

--- app.js
+++ app.js
@@ -1,47 +1,55 @@
-//we need a constructor function that take in the input from the index.html push to an array and store in local storage
-//event listener that listens for add to cart button
-//event handler that takes in the user input from the drop down and the quantity and pushes it to a constructor
-
-
-//what global variables do we need to define
 'use strict';
 
-
+///////////////////////////// DEFINE GLOBAL VARIABLES ////////////////////////////
 // declare an array that holds the images
 var btn = document.getElementById('addToCart');//on index
-var itemName = document.getElementById('product'); //on index
-var itemQuantity = document.getElementByType('number'); //on index
 var orders = document.getElementById('orders'); //on cart
+//console.log(itemQuantity);
 
-
-//this declares but also checks if the array that will hold all the constructor info is there to begin with
-if(localStorage.list) {
-  var itemArray = localStorage.list.split(',');
+if(localStorage.itemArray) {
+  var itemArray = localStorage.itemArray.split(',');
 } else {
-  var itemArray = [];// this holds our constructor object instances based on user choice from index.html
+  var itemArray = [];
 }
 
-//save the state of our application
-//attach it to some sort of event listener
-//grab our list and push the value of what of what we type in to localStorage
-//every time you add an item your item will go to localStorage
+///////////////////////////// HANDLER FUNCTION ////////////////////////////
+function handleProductSubmit(event) {
+  console.log(event);
+  event.preventDefault();
+
+  var itemName = document.getElementById('product').value; //on index
+  var itemQuantity = document.getElementsByName('quantity'); //on index
+  console.log(itemName);
+  console.log(itemQuantity);
+  itemArray.push(new Constructor(itemName, itemQuantity));
+
+  function Constructor(productName, productQuantity) {
+    this.productName = productName;
+    this.productQuantity = productQuantity;
+  }
+  save();
+  console.log(itemName);
+  create();
+  function create() {
+    var val = itemArray.productName;
+    //var val2 = itemArray.productQuantity;
+    var item = document.createElement('li');
+    item.appendChild(document.createTextNode(val));
+    orders.appendChild(item);
+    item.value = '';
+  }
+  console.log(itemArray);
+}
+
+///////////////////////////// SAVE FUNCTION ////////////////////////////
 
 function save() {
-  itemArray.push(itemName.value);
   localStorage.itemArray = itemArray;
   console.log('list arr:', itemArray);
   console.log('localStorage list:', localStorage.itemArray);
 }
 
-//the create function will call a constructor function that will take the user input
-function create() {
-  var val = itemName.value;
-  var item = document.createElement('li');
-  item.appendChild(document.createTextNode(val));
-  orders.appendChild(item);
-  itemName.value = '';
-}
-
+///////////////////////////// LOAD FUNCTION ////////////////////////////
 function load() {
   if(localStorage.itemArray) {
     var item;
@@ -52,11 +60,9 @@ function load() {
     }
   }
 }
-
-function render() {
-  save();
-  create();
-}
 load();
-btn.addEventListener('click', render);
+console.log('list arr:', itemArray);
+///////////////////////////// EVENT LISTENER ////////////////////////////
+btn.addEventListener('click', handleProductSubmit);
 
+//button to clear locaStorage in a code demo
