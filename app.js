73fbee62c9c778f9209bb959006cbc005fcda454
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-02-21 17:59:27 -0800

    this is for me and Michael lennerblom

--- app.js
+++ app.js
@@ -12,25 +12,28 @@ if(localStorage.itemArray) {
   var itemArray = [];
 }
 
+////cart constructor function. moved outside of handler
+function Constructor(productName, productQuantity) {
+  this.productName = productName;
+  this.productQuantity = productQuantity;
+  itemArray.push(this);
+  console.log(itemArray);
+}
 ///////////////////////////// HANDLER FUNCTION ////////////////////////////
 function handleProductSubmit(event) {
   console.log(event);
-  event.preventDefault();
-
   var itemName = document.getElementById('product').value; //on index
-  var itemQuantity = document.getElementsByName('quantity'); //on index
+  var itemQuantity = parseInt(document.getElementsById('quantity').value); //on index
   console.log(itemName);
   console.log(itemQuantity);
-  itemArray.push(new Constructor(itemName, itemQuantity));
 
-  function Constructor(productName, productQuantity) {
-    this.productName = productName;
-    this.productQuantity = productQuantity;
-  }
+  new Constructor(itemName, itemQuantity);//moved the push to the constructor outside the handler
+
+
   save();
   console.log(itemName);
   create();
-  function create() {
+  function create() {// dont want in handler // two handler one for adding to cart only storage and the other to switch to the other page adn appendchild stuff FROM the locale storage
     var val = itemArray.productName;
     //var val2 = itemArray.productQuantity;
     var item = document.createElement('li');
@@ -56,7 +59,7 @@ function load() {
     for (var x = 0; x < itemArray.length; x++) {
       item = document.createElement('li');
       item.appendChild(document.createTextNode(itemArray[x]));
-      orders.appendChild(item);
+      orders.appendChild(item);// make sure local storage
     }
   }
 }
